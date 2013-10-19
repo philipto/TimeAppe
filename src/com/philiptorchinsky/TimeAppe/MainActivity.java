8185068d0124f356d3648f519b2850625c5e3917
Philip <philip.torchinsky@gmail.com>
2013-10-19 19:03:56 +0300

    #AA-11 fixed

--- src/com/philiptorchinsky/TimeAppe/MainActivity.java
+++ src/com/philiptorchinsky/TimeAppe/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends ListActivity {
 
         Cursor c = dh.getAll();
 
-        int i = 0;
+        int i = 3;
         c.moveToFirst();
         if (!c.isAfterLast()) {
             do {
