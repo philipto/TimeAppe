d91ae95893e99db87e007ffeeb7167a0fe3e9743
Philip <philip.torchinsky@gmail.com>
2014-04-22 18:25:56 +0400

    #AA-44 fixed replaced 0 with 6

--- src/com/philiptorchinsky/TimeAppe/MainActivity.java
+++ src/com/philiptorchinsky/TimeAppe/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends ListActivity {
         Cursor c = dh.getAll();
 
 
-        int i = 6;
+        int i = 9;
 
         c.moveToFirst();
         if (!c.isAfterLast()) {
